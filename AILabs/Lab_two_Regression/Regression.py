cc3c49241f44e5812aaa5064461a329a633c57ec
storkprg <nikdfil@yandex.ru>
2023-03-10 16:11:24 +0300

    Task 2: Regression vers. 2

--- AILabs/Lab_two_Regression/Regression.py
+++ AILabs/Lab_two_Regression/Regression.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import sklearn as sk
 import sklearn.preprocessing
+import sklearn.compose
 import sklearn.linear_model
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -19,18 +20,23 @@ def infAboutWines():
     print(f"{data.isnull().sum()}")
 
 def linearRegressionML():
+    # Visualize the signs on the histograms
     data.hist(bins=50, figsize=(20, 15))
     plt.show()
 
+    # Visualize the signs on the distribution
+    sns.set_style("whitegrid")
+    sns.set(rc={'figure.figsize': (22, 12)})
     sns.pairplot(data=data, kind='scatter', diag_kind='kde')
     plt.show()
 
+    # Let's split the data into a sample for training and testing
     df_train, df_test = sk.model_selection.train_test_split(data, train_size=0.2)
 
     Y = df_train["quality"]
     Y_t = df_test["quality"]
 
-    numeric = ['fixed acidity', 'volatile acidity', 'citric acid','residual sugar', 'chlorides', 'free sulfur dioxide','total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
+    numeric = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
 
     categorical = ['type']
 
@@ -41,9 +47,10 @@ def linearRegressionML():
     X_t = df_test[numeric]
     Y_t_pred = model.predict(X_t)
 
-    print(f"MSE={sk.metrics.mean_squared_error(Y_t, Y_t_pred)}, \
-        MAE={sk.metrics.mean_absolute_error(Y_t, Y_t_pred)}, \
-        MAE(%)={sk.metrics.mean_absolute_percentage_error(Y_t, Y_t_pred)}")
+    # The output of the trained model is based only on numerical features
+    print(f"MSE = {sk.metrics.mean_squared_error(Y_t, Y_t_pred)}, "
+          f"MAE = {sk.metrics.mean_absolute_error(Y_t, Y_t_pred)}, "
+          f"MAE(%) = {sk.metrics.mean_absolute_percentage_error(Y_t, Y_t_pred)}")
 
     transformer = sk.compose.make_column_transformer(
         (sk.preprocessing.OneHotEncoder(), ['type']),
@@ -59,9 +66,10 @@ def linearRegressionML():
     X_t = transformer.transform(df_test[numeric + categorical])
     Y_t_pred = model.predict(X_t)
 
-    print(f"MSE={sk.metrics.mean_squared_error(Y_t, Y_t_pred)}, \
-        MAE={sk.metrics.mean_absolute_error(Y_t, Y_t_pred)}, \
-        MAE(%)={sk.metrics.mean_absolute_percentage_error(Y_t, Y_t_pred)}")
+    # Output of the trained model based on numerical and categorical features
+    print(f"\nMSE = {sk.metrics.mean_squared_error(Y_t, Y_t_pred)}, "
+          f"MAE = {sk.metrics.mean_absolute_error(Y_t, Y_t_pred)}, "
+          f"MAE(%) = {sk.metrics.mean_absolute_percentage_error(Y_t, Y_t_pred)}")
 
 
 while True:
